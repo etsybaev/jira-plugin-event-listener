d6b3227490878be10ff8b816475dc70685b23c98
ievgenii.tsybaiev <ievgenii.tsybaiev>
2019-11-12 18:22:10 +0200

    Added test MoneyCustomField

---
+++ src/main/java/com/aval/jira/plugins/customfield/MoneyCustomField.java
@@ -0,0 +1,62 @@
+package com.aval.jira.plugins.customfield;
+
+import com.atlassian.jira.issue.customfields.impl.AbstractSingleFieldType;
+import com.atlassian.jira.issue.customfields.impl.FieldValidationException;
+import com.atlassian.jira.issue.customfields.manager.GenericConfigManager;
+import com.atlassian.jira.issue.customfields.persistence.CustomFieldValuePersister;
+import com.atlassian.jira.issue.customfields.persistence.PersistenceFieldType;
+import com.atlassian.plugin.spring.scanner.annotation.component.Scanned;
+import com.atlassian.plugin.spring.scanner.annotation.imports.JiraImport;
+
+import java.math.BigDecimal;
+
+@Scanned
+public class MoneyCustomField extends AbstractSingleFieldType<BigDecimal> {
+
+    public MoneyCustomField(
+            @JiraImport CustomFieldValuePersister customFieldValuePersister,
+            @JiraImport GenericConfigManager genericConfigManager) {
+
+        super(customFieldValuePersister, genericConfigManager);
+    }
+
+    @Override
+    public String getStringFromSingularObject(final BigDecimal singularObject) {
+        if (singularObject == null)
+            return null;
+        else
+            return singularObject.toString();
+    }
+
+    @Override
+    public BigDecimal getSingularObjectFromString(final String string) throws FieldValidationException {
+        if (string == null)
+            return null;
+        try {
+            BigDecimal decimal = new BigDecimal(string);
+            if (decimal.scale() > 2) {
+                throw new FieldValidationException(
+                        "Maximum of 2 decimal places are allowed.");
+            }
+            return decimal.setScale(2);
+        } catch (NumberFormatException ex) {
+            throw new FieldValidationException("Not a valid number.");
+        }
+    }
+
+    @Override
+    protected PersistenceFieldType getDatabaseType() {
+        return PersistenceFieldType.TYPE_LIMITED_TEXT;
+    }
+
+    @Override
+    protected BigDecimal getObjectFromDbValue(final Object databaseValue)
+            throws FieldValidationException {
+        return getSingularObjectFromString((String) databaseValue);
+    }
+
+    @Override
+    protected Object getDbValueFromObject(final BigDecimal customFieldObject) {
+        return getStringFromSingularObject(customFieldObject);
+    }
+}
